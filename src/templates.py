acb731ce37e7008a608880b9e396d9d077e6afe4
matv864 <matv864@gmail.com>
2025-04-08 19:34:39 +1000

    try to make https static

--- src/templates.py
+++ src/templates.py
@@ -1,5 +1,18 @@
+from typing import Any
+
+from fastapi import Request
 from fastapi.templating import Jinja2Templates
 from src.settings import settings
 
+
 # Создаем экземпляр Jinja2Templates для использования в приложении
 templates = Jinja2Templates(directory=settings.TEMPLATES_DIR)
+
+def https_url_for(request: Request, name: str, **path_params: Any) -> str:
+
+    http_url = request.url_for(name, **path_params)
+
+    # Replace 'http' with 'https'
+    return http_url.replace("http", "https", 1)
+
+templates.env.globals["https_url_for"] = https_url_for
